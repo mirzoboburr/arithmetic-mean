55c8272550cb87acb3ad9a9e4d3720932bf4b609
Bobur <bobur927788575@outlook.com>
2022-05-03 16:39:50 +0500

    arithmetic mean
    Calculate arithmetic mean scanning numbers from file

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    File file = new File("input.txt");
+
+        try {
+        Scanner scanner = new Scanner(file);
+
+        String line = scanner.nextLine();
+        String[] numbers = line.split(" ");
+
+        int sum = 0;
+        for (String number : numbers)
+            sum += Integer.parseInt(number);
+
+        double result = sum / (double)numbers.length;
+
+            System.out.printf(result + " --> %.3f", result);
+        } catch (FileNotFoundException e) {
+            System.out.println("idi nahoy");
+        }
+    }
+}
+//   if 5 > 2 :
+//        print("Five is greater than two!")
+
+//  if( 5 > 2 ) {
+//    System.out.println ("Five is greater than two!");
+//   }
